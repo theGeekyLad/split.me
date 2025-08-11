51286a786cd0671a62e4ab04b0751787b2e905ef
Rahul <rahul.pillai03@gmail.com>
2025-08-10 21:19:00 -0700

    Add axios and set up basic list groups API call

---
+++ src/services/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_URL = 'https://secure.splitwise.com';
+const BEARER_TOKEN = 'u3ol04rmRnMsn1I6eA8zpAFSX1hJt9pJNHTBoX4h';
+
+export const getGroups = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/api/v3.0/get_groups`, {
+      headers: {
+        Authorization: `Bearer ${BEARER_TOKEN}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
