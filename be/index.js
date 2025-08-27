5a9d1c21afb8ff84afadae0a04c57d873e7dce77
Rahul <rahul.pillai03@gmail.com>
2025-08-26 19:41:24 -0700

    [BE] Accept API key in all service calls

--- be/index.js
+++ be/index.js
@@ -7,15 +7,15 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 const API_URL = 'https://secure.splitwise.com';
-const BEARER_TOKEN = 'u3ol04rmRnMsn1I6eA8zpAFSX1hJt9pJNHTBoX4h';
 
 app.use(express.json());
 
 app.get('/get_groups', async (req, res) => {
+  const apiKey = req.query.apiKey;
   try {
     const response = await axios.get(`${API_URL}/api/v3.0/get_groups`, {
       headers: {
-        Authorization: `Bearer ${BEARER_TOKEN}`,
+        Authorization: `Bearer ${apiKey}`,
       },
     });
     res.status(response.status).json(response.data);
@@ -29,10 +29,11 @@ app.get('/get_groups', async (req, res) => {
 
 app.get('/get_group/:id', async (req, res) => {
   const groupId = req.params.id;
+  const apiKey = req.query.apiKey;
   try {
     const response = await axios.get(`${API_URL}/api/v3.0/get_group/${groupId}`, {
       headers: {
-        Authorization: `Bearer ${BEARER_TOKEN}`,
+        Authorization: `Bearer ${apiKey}`,
       },
     });
     res.status(response.status).json(response.data);
@@ -44,13 +45,14 @@ app.get('/get_group/:id', async (req, res) => {
 
 
 app.post('/create_expense', async (req, res) => {
+  const apiKey = req.query.apiKey;
   try {
     const response = await axios.post(
       `${API_URL}/api/v3.0/create_expense`,
       req.body,
       {
         headers: {
-          Authorization: `Bearer ${BEARER_TOKEN}`,
+          Authorization: `Bearer ${apiKey}`,
           'Content-Type': 'application/json',
         },
       }
